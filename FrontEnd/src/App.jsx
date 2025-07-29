84fd352030b9c592ce35c47b011a2a07f16424e4
pankajyofficiale <pankajyofficial@gmail.comm>
2025-07-30 00:55:38 +0530

    component create

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Component/Header.jsx";
+import Footer from "./Component/Footer.jsx";
+import Body from "./Component/Body.jsx";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+      <Body/>
+    </>
+  );
+}
+
+export default App;
